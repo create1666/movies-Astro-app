2c7fd189b7fc173758eff15a64dbb4d469116904
create1666 <create1666@gmail.com>
2021-09-04 15:34:09 +0100

    hover-cards

--- src/Components/lisItem/ListItem.jsx
+++ src/Components/lisItem/ListItem.jsx
@@ -1,18 +1,40 @@
 /** @format */
 
-import { PlayArrow } from "@material-ui/icons";
+import {
+  Add,
+  PlayArrow,
+  ThumbDownAltOutlined,
+  ThumbUpAltOutlined,
+} from "@material-ui/icons";
+import { useState } from "react";
 import { imgBg } from "./Assets/img";
 import "./ListItem.scss";
 
-export const ListItem = () => {
+export const ListItem = ({ index }) => {
+  const [isHovered, setIsHovered] = useState(false);
   const { img } = imgBg;
   return (
-    <div className="listItem ">
+    <div
+      className="listItem "
+      style={{ left: isHovered && index * 225 - 50 }}
+      onMouseOver={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <img src={img} alt="" className="listItemImg" />
       <div className="infoIcons">
         <div className="icons">
           <PlayArrow />
+          <Add />
+          <ThumbUpAltOutlined />
+          <ThumbDownAltOutlined />
         </div>
+        <div className="itemInfoTop">
+          <span>1 hour 14 mins</span>
+          <span className="limit">+16</span>
+          <span>1999</span>
+        </div>
+        <div className="desc"></div>
+        <div className="genre">Action</div>
       </div>
     </div>
   );
--- src/Components/list/List.jsx
+++ src/Components/list/List.jsx
@@ -7,7 +7,7 @@ import {
 import React, { useRef, useState } from "react";
 import "./List.scss";
 import { ListItem } from "../lisItem/ListItem";
-import { imgBg } from "../lisItem/Assets/img";
+// import { imgBg } from "../lisItem/Assets/img";
 
 const List = () => {
   const listRef = useRef();
@@ -20,11 +20,11 @@ const List = () => {
 
     if (paramet === "left" && sliderNumber > 0) {
       setSliderNumber(sliderNumber - 1);
-      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+      listRef.current.style.transform = `translateX(${225 + distance}px)`;
     }
     if (paramet === "right" && sliderNumber < 5) {
       setSliderNumber(sliderNumber + 1);
-      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      listRef.current.style.transform = `translateX(${-225 + distance}px)`;
     }
     console.log("ex=", distance);
   };
@@ -42,16 +42,16 @@ const List = () => {
           />
         </div>
         <div className="container" ref={listRef}>
-          <ListItem {...imgBg} />
-          <ListItem {...imgBg} />
-          <ListItem {...imgBg} />
-          <ListItem {...imgBg} />
-          <ListItem {...imgBg} />
-          <ListItem {...imgBg} />
-          <ListItem {...imgBg} />
-          <ListItem {...imgBg} />
-          <ListItem {...imgBg} />
-          <ListItem {...imgBg} />
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />
         </div>
         <div>
           {" "}
