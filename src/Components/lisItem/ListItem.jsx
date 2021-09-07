1af01778270d3f84dec04bbdb6f04a79beb17d5d
create1666 <create1666@gmail.com>
2021-09-07 19:55:11 +0100

    latest

--- src/Components/lisItem/ListItem.jsx
+++ src/Components/lisItem/ListItem.jsx
@@ -13,29 +13,38 @@ import "./ListItem.scss";
 export const ListItem = ({ index }) => {
   const [isHovered, setIsHovered] = useState(false);
   const { img } = imgBg;
+  const trailer = "https://vimeo.com/376002591";
+
   return (
     <div
       className="listItem "
-      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      style={{ left: isHovered && index * 325 - 50 + index * 2.5 }}
       onMouseOver={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <img src={img} alt="" className="listItemImg" />
-      <div className="infoIcons">
-        <div className="icons">
-          <PlayArrow />
-          <Add />
-          <ThumbUpAltOutlined />
-          <ThumbDownAltOutlined />
-        </div>
-        <div className="itemInfoTop">
-          <span>1 hour 14 mins</span>
-          <span className="limit">+16</span>
-          <span>1999</span>
-        </div>
-        <div className="desc"></div>
-        <div className="genre">Action</div>
-      </div>
+
+      {isHovered && (
+        <>
+          {" "}
+          <video src={trailer} autoPlay={true} loop />
+          <div className="infoIcons">
+            <div className="icons">
+              <PlayArrow />
+              <Add />
+              <ThumbUpAltOutlined />
+              <ThumbDownAltOutlined />
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>1999</span>
+            </div>
+            <div className="desc"></div>
+            <div className="genre">Action</div>
+          </div>{" "}
+        </>
+      )}
     </div>
   );
 };
